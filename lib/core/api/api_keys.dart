d9470e58f3b6d77177e458bf86e8653158d2a346
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-05-13 14:22:19 +0300

    refactor keys to stripekeys

--- lib/core/api/api_keys.dart
+++ lib/core/api/api_keys.dart
@@ -1,6 +1,6 @@
 abstract class ApiKeys {
-  static String publishableKey =
+  static String stripePublishableKey =
       'pk_test_51PFHAcBtDg1Q0pYde5QMUF5XeG6AjoUmnR1O921GY7pzKDKnln1AaGmYd9XKP8HV2ajtyVIn5YkpDXrtUcBbTwjW00N2BkISH0';
-  static String secretKey =
+  static String stripeSecretKey =
       'sk_test_51PFHAcBtDg1Q0pYdZp3HjdvRubpUnMnH3NGadkfhX6MgcuwE8NytBNjvnooqVpwDEU2rtoULKM1rywjm2n5pTf8k00f9O3ew6h';
 }
